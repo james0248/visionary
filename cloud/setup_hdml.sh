1738685123cf5a24f38580e952e7326eca6f1756
Hyeonseok Jung <hsjames0248@gmail.com>
2026-07-27 11:54:30 +0900

    Point checkpoint paths at existing buckets
    gs://visionary-exp does not exist. Atari goes to dream-atari-bucket, SO-101
    to visionary-robot-bucket. Atari data disks live at the bucket root.

--- cloud/setup_hdml.sh
+++ cloud/setup_hdml.sh
@@ -29,7 +29,7 @@ Example:
     --zone us-east1-d \
     --size-gb 200 \
     --throughput 2000 \
-    --gcs-path 'gs://visionary-exp/dream-arcade/data/*_tokenizer'
+    --gcs-path 'gs://dream-atari-bucket/data/*_tokenizer'
 
 Example with exact sources:
   cloud/setup_hdml.sh \
@@ -37,8 +37,8 @@ Example with exact sources:
     --zone us-east1-d \
     --size-gb 200 \
     --throughput 2000 \
-    --gcs-path gs://visionary-exp/dream-arcade/data/qbert_dynamics/ \
-    --gcs-path gs://visionary-exp/dream-arcade/data/seaquest_dynamics/
+    --gcs-path gs://dream-atari-bucket/data/qbert_dynamics/ \
+    --gcs-path gs://dream-atari-bucket/data/seaquest_dynamics/
 
 The script:
   1. Deletes an existing detached disk with the same name, if present.
